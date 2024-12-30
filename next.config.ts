ae8b22b2822a68a189c8977bc02cd9a9f731ebb1
Jacob <kontakt.jakub.wronski@gmail.com>
2024-12-30 19:56:16 +0100

    Remove base path and unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/joanna-sierak-homepage",
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  output: "export"
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
