7434cc4f788f7b44c7d40ebb179972711b08adb2
Jacob <kontakt.jakub.wronski@gmail.com>
2024-12-30 20:04:42 +0100

    Add unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/joanna-sierak-homepage",
-  output: "export"
-  // images: {
-  //   unoptimized: true,
-  // },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
