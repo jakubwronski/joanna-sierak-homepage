0b46812abda328f93facb60bd4511146426f9c39
Jacob <kontakt.jakub.wronski@gmail.com>
2026-02-25 12:44:24 +0100

    Build static content

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pzmNMB0RMy7QRzn+RQ5PYaHlkHfU1GvdtLJqTeqcvhk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
